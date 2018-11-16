ae8d6ad4d24e98c6d131d7da845dcf95d7258138
Aurélien Ammeloot <559069+aammeloot@users.noreply.github.com>
2018-11-16 12:13:31 +0000

    Boolean unsafe version

---
+++ menu-input-boolean-validation-unsafe.py
@@ -0,0 +1,38 @@
+# This is an example of a menu interface in Python.
+# This version includes an input validation for integers.
+# This code is unsafe, i.e. if there is a text input
+# it is likely to crash.
+
+# Variable: choice, integer
+choice = 0
+# Flag: input is valid, boolean, default value is False
+input_valid = False
+
+# We are sure to enter the loop because we explicitly
+# set up the flag as False on line 9
+while not input_valid:
+    # Print the menu
+    print("Welcome to the jungle")
+    print("1 - Coffee ping")
+    print("2 - Tea traceroute")
+    print("3 - Beer netstat")
+    # Read the input and convert to an integer
+    choice = int(input("Please enter your choice: "))
+    # Set up the flag if the choice is in the boundaries
+    if choice >=1 and choice <=3:
+        # By setting the flag to True we stop the loop
+        input_valid = True
+    else:
+        print("Invalid choice, enter something between 1 and 3")
+
+
+# Code branching the different menu actions
+if choice == 1:
+    print("Coffee")
+elif choice == 2:
+    print("Tea")
+elif choice == 3:
+    print("Beer")
+else:   # Default for a non-recongnised value
+    print("You did not enter a valid choice")
+
